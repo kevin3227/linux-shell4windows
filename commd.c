4f9bba40f61913272a399cafb8642a7227c3680a
kevin3227 <58798338+kevin3227@users.noreply.github.com>
2021-12-21 00:14:38 +0800

    Add files via upload

---
+++ commd.c
@@ -0,0 +1,5 @@
+/* implementation of the commands */
+
+#include<windows.h>
+
+/* TODO */
---
+++ main.c
@@ -0,0 +1,48 @@
+/* skeleton of the simplified shell */
+
+#include<windows.h>
+
+HANDLE	handle_in;
+HANDLE	handle_out;
+DWORD dw;
+
+int console();
+/* TODO */
+
+int console() {
+	// I/O handle Windows API
+	handle_in = GetStdHandle(STD_INPUT_HANDLE);
+	handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int control = 1;
+	// main loop
+	while (control) {
+		// get current directory
+		char *current_directory = (char*)malloc(sizeof(char) * MAX_PATH);
+		GetCurrentDirectory(MAX_PATH, current_directory);
+		// display current directory
+		WriteConsole(handle_out, current_directory, strlen(current_directory), &dw, NULL);
+		WriteConsole(handle_out, "$ ", strlen("$ "), &dw, NULL);
+
+		// define a buffer to receive command and argv
+		char *command = (char*)malloc(sizeof(char) * 512);  //1KB
+		// char *argv[8];  //max 8 argv(8*64 = 512)
+		// int *argc = (int*)malloc(sizeof(int));
+
+		// get command form console input buffer
+		ReadConsole(handle_in, command, 512, &dw, NULL);
+		// getArgv(command, argv, argc);
+	}
+
+	CloseHandle(handle_out);
+	CloseHandle(handle_in);
+
+	/* TODO */
+
+	return 0;
+}
+
+int main() {
+	console();
+	return 0;
+}
